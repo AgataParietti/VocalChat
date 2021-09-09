9e61c9fc36999437c3d1e099a6fad7856d68426c
Agata Parietti <agata.parietti@stud.unifi.it>
2021-09-09 18:40:03 +0200

    Add files via upload

--- server/actions.php
+++ server/actions.php
@@ -1,14 +1,10 @@
 <?php
 
-header('Content-Type: text/json');
 require_once("config.php");
 
 $action = $_POST['action'];
 
 switch ($action) {
-    case "load" :
-        getPlaceId();
-        break;
     case "save" :
         saveAudio();
         break;
@@ -16,92 +12,53 @@ switch ($action) {
         getAudios();
         break;
 }
-//non sono sicura ci serva
-function getPlaceId()
-{
-    $query_string = 'SELECT placeId FROM audios-db GROUP BY placeId';
-    $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
-    $result = $mysqli->query($query_string);
 
-    $places_id = array();
+function saveAudio() {
+    $placeId = $_POST['pl_id'];
+    $vote = $_POST['vote'];
+    $audio = $_POST['audio'];
 
-    while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
+    $n = 'audio : ' . $audio;
+    printf("%%s = '%s'\n", $n); // string representation
 
-        $pl_id = $row['placeId'];
 
-        $pl_id_arr = array('pl_id' => $pl_id);
-        array_push($places_id, $pl_id_arr);
-    }
+    $audio = str_replace('data:audio/wav;base64,', '', $audio);
+    $audio = str_replace(' ', '+', $audio);
 
-    $response = array('places_id' => $places_id, 'type' => 'load');
-    echo json_encode($response);
-}
-
-//non ho provato, ma sono fiduciosa
-function saveAudio()
-{
-    $pl_id = $_POST['pl_id'];
-    $aud = $_POST['aud'];
-    $nome = $_POST['nome'];
-    //$vote = $_POST['vote'];
-
-    $aud = str_replace('data:audio/mp3;base64', '', $aud);
-    $aud = str_replace(' ', '+', $aud);
-
-    // create an image file
-    $fp = fopen("tmp.mp3", "w+");
-    // write the data in image file
-    fwrite($fp, base64_decode($aud));
-    // close an open file pointer
+    $fp = fopen('../Audio/tmp.wav', 'w+');
+    fwrite($fp, base64_decode($audio));
     fclose($fp);
 
-    $query_string = 'INSERT INTO audios-db (placeId, audio, nome) VALUES(?,?,?)';
+    $query_string = "INSERT INTO `audios`( `placeId`, `audio`, `vote`) VALUES (?,?,?)";
     $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
     $query = $mysqli->prepare($query_string);
     $null = NULL;
-    $query->bind_param("ddbs", $pl_id, $null, $nome);
-    $query->send_long_data(2, file_get_contents('tmp.mp3'));
+    $query->bind_param("sbi", $placeId, $null, $vote);
+    $query->send_long_data(1,  file_get_contents('../Audio/tmp.wav'));
     $query->execute();
-
-    $id = $mysqli->insert_id;
-
-    $query = $mysqli->prepare('SELECT nome FROM audios-db WHERE id=?');
-    $query->bind_param("i", $id);
-    $query->execute();
-
-    $result = $query->get_result();
-    $row = $result->fetch_assoc();
-
-    $line = array('nome' => $row['nome']);
-    $lines = array();
-    array_push($lines, $line);
-
-    $response = array('lines' => $lines, 'type' => 'save');
-    echo json_encode($response);
 }
 
-//sicuramente non funziona
-function getAudios()
-{
-    $title = $_POST['title'];
+function getAudios() {
+    $placeId = $_POST['pl_id'];
 
-    $query_string = 'SELECT audio FROM audios-db WHERE nome=?';
+    $query_string = 'SELECT `audio` FROM `audios` WHERE `placeId`=? ORDER BY `audio`';
     $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
     $query = $mysqli->prepare($query_string);
-    $query->bind_param("s", $title);
+    $query->bind_param("s", $placeId);
     $query->execute();
-
-    $images = array();
 
     $query->store_result();
     $query->bind_result($gallery);
 
+    $audios = array();
+
     while ($query->fetch()) {
         $aud = array('aud' => base64_encode($gallery));
-        array_push($images, $img);
+        array_push($audios, $aud);
     }
 
-    $response = array('audios' => $audios, 'title' => $title, 'type' => 'get');
+    header('Content-Type: text/json');
+    $response = array('audios' => $audios, 'type' => 'get');
     echo json_encode($response);
 }
 
--- server/config.php
+++ server/config.php
@@ -1,7 +1,5 @@
 <?php
-// defines database connection data
 define('DB_HOST', 'localhost');
 define('DB_USER', 'angiologiandonati');
 define('DB_PASSWORD', '');
 define('DB_DATABASE', 'my_angiologiandonati');
-?>
